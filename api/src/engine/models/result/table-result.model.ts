7838f9a8bcbf46c125ac3b7737c7158ed8fb8bda
stevereis <stevereis93@gmail.com>
2021-09-28 11:43:31 +0200

    Add grouping property on results

--- api/src/engine/models/result/table-result.model.ts
+++ api/src/engine/models/result/table-result.model.ts
@@ -1,8 +1,9 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { MetaData } from './common/metadata.model';
+import { Result } from './result.model';
 
 @ObjectType()
-export class TableResult {
+export class TableResult extends Result {
   @Field()
   name: string;
 
