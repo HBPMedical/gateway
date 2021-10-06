aba36a17396810c4d9448b6f04d457a0d65e71b1
stevereis <stevereis93@gmail.com>
2021-10-06 13:27:18 +0200

    Add GroupsResult in ResultUnion

--- api/src/engine/models/result/common/result-union.model.ts
+++ api/src/engine/models/result/common/result-union.model.ts
@@ -1,16 +1,22 @@
 import { createUnionType } from '@nestjs/graphql';
+import { GroupsResult } from '../groups-result.model';
 import { RawResult } from '../raw-result.model';
 import { TableResult } from '../table-result.model';
 
 export const ResultUnion = createUnionType({
   name: 'ResultUnion',
-  types: () => [TableResult, RawResult],
+  types: () => [TableResult, RawResult, GroupsResult],
   resolveType(value) {
     if (value.headers) {
       return TableResult;
     }
+
     if (value.listMax) {
       return RawResult;
+    }
+
+    if (value.group) {
+      return GroupsResult;
     }
 
     return null;
