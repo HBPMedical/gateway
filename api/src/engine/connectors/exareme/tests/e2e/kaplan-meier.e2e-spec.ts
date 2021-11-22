7e76b926bb72849c8ade190d604b31fe4756e0fa
stevereis <stevereis93@gmail.com>
2021-11-22 18:02:58 +0100

    cleaning imports

--- api/src/engine/connectors/exareme/tests/e2e/kaplan-meier.e2e-spec.ts
+++ api/src/engine/connectors/exareme/tests/e2e/kaplan-meier.e2e-spec.ts
@@ -1,6 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ParamType } from 'src/engine/models/experiment/input/algorithm-parameter.input';
-import { RawResult } from 'src/engine/models/result/raw-result.model';
 import { AppModule } from '../../../../../main/app.module';
 import { ENGINE_SERVICE } from '../../../../engine.constants';
 import { IEngineService } from '../../../../engine.interfaces';
