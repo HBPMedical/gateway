d6a3d0b0332d4208deef6f0f72b536c6e29028bf
stevereis <stevereis93@gmail.com>
2022-03-01 19:22:29 +0100

    wip: Add static assets

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path/posix';
 import { AppModule } from './main/app.module';
 
 const CORS_URL = process.env.CORS_URL ?? process.env.ENGINE_BASE_URL;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
       credentials: true,
       origin: [
@@ -14,6 +16,11 @@ async function bootstrap() {
       ],
     },
   });
+
+  app.useStaticAssets(join(__dirname, '..', 'assets'), {
+    prefix: '/assets/',
+  });
+
   await app.listen(process.env.GATEWAY_PORT);
 }
 bootstrap();
