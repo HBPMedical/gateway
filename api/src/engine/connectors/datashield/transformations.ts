c5ec32587c3efa81234988012ca2fa395b24b244
Steve Reis <stevereis93@gmail.com>
2022-03-01 15:52:11 +0000

    Add datashield support for histograms and descriptives stats

--- api/src/engine/connectors/datashield/transformations.ts
+++ api/src/engine/connectors/datashield/transformations.ts
@@ -22,7 +22,91 @@ export const transformToDomains = jsonata(`
     },
     "variables": $distinct(groups.variables).{
         "id": $,
-        "label": $
+        "label": $trim($replace($ & '', '.', ' ')),
+        "type": "Number"
     }
 }
 `);
+
+export const transformToHisto = jsonata(`
+(
+    $nbBreaks := $count(breaks);
+
+    {
+    "chart": {
+      "type": 'column'
+    },
+    "legend": {
+      "enabled": false
+    },
+    "series": [
+      {
+        "data": counts,
+        "dataLabels": {
+          "enabled": true
+        }
+      }
+    ],
+    "title": {
+      "text": title ? title : ''
+    },
+    "tooltip": {
+      "enabled": true
+    },
+    "xAxis": {
+      "categories": breaks#$i[$i < $nbBreaks-1].[$ & ' - ' & %.*[$i+1]]
+    },
+    "yAxis": {
+      "min": 0,
+      "minRange": 0.1,
+      "allowDecimals": true
+    }
+})
+  `);
+
+export const transformToTable = jsonata(`
+{
+    "name": "Descriptive Statistics",
+    "headers": $append(title, ['5%','10%','25%','50%','75%','90%','95%','Mean']).{
+        "name": $,
+        "type": "string"
+    },
+    "data": $.message.$each(function($v, $k) {
+           $append($k,$v)
+        })
+}
+`);
+
+/*export const transformToTable = jsonata(`
+(
+    $params := ["xname", "equidist", "breaks"];
+    $concat := function($i, $j) {
+        $append($i, $j)
+    };
+
+    {
+        "name": "test",
+        "headers": $append('', $.*[0].breaks).{
+            "name": $,
+            "type": "string"
+        },
+        "data": $.$each(function($v, $k) {
+           [[$k],$v.$each(function($v, $k) {$not($k in $params) ? $append($k, $v) : undefined})]
+        }) ~> $reduce($concat, [])
+    }
+)
+`);*/
+
+/* export const transformToTable = jsonata(`
+(
+    $params := ["xname", "equidist", "breaks"];
+
+    $.$each(function($v, $k) {
+        {
+            "name": $k,
+            "headers": $v.breaks,
+            "data": $v.$each(function($v, $k) {$not($k in $params) ? $append($k, $v) : undefined})
+        }
+    })
+)
+`); */
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './main/app.module';
 
+const CORS_URL = process.env.CORS_URL ?? process.env.ENGINE_BASE_URL;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      credentials: true,
+      origin: [
+        /http:\/\/localhost($|:\d*)/,
+        /http:\/\/127.0.0.1($|:\d*)/,
+        CORS_URL,
+      ],
+    },
+  });
   await app.listen(process.env.GATEWAY_PORT);
 }
 bootstrap();
