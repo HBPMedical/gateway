08d8a53fbc884e25b144d2f544836a4e67ea8232
Steve Reis <stevereis93@gmail.com>
2022-03-08 16:50:19 +0000

    feat: Add branding and configuration for connectors

---
+++ api/src/engine/assets.service.ts
@@ -0,0 +1,56 @@
+import { Inject, Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import { join } from 'path/posix';
+import { ENGINE_MODULE_OPTIONS } from './engine.constants';
+import { IEngineOptions } from './engine.interfaces';
+
+@Injectable()
+export class AssetsService {
+  constructor(
+    @Inject(ENGINE_MODULE_OPTIONS)
+    private readonly engineOptions: IEngineOptions,
+  ) {}
+
+  /**
+   * Get internal asset file from filename
+   * @param filename
+   * @returns string if file is found undefined otherwise
+   */
+  getAssetFile(filename: string): string | undefined {
+    // Construct file path based on the connector id
+    let filePath = join(
+      process.cwd(),
+      'assets/engines',
+      this.engineOptions.type,
+      filename,
+    );
+
+    // if file doesn't exist for the current connector fallback to default
+    if (!fs.existsSync(filePath)) {
+      filePath = join(
+        process.cwd(),
+        'assets/engines/default',
+        filename.toLowerCase(),
+      );
+    }
+
+    return fs.existsSync(filePath) ? filePath : undefined;
+  }
+
+  /**
+   * Get markdown enhanced with custom variable
+   * `$ASSETS_URL$` will be transform with backend url
+   * @param filename name of the asset
+   * @param baseurl url of the backend service
+   * @returns string markdown
+   */
+  getMarkdown(filename: string, baseurl: string): string {
+    const filePath = this.getAssetFile(filename);
+
+    if (!filePath) return '';
+
+    const md = fs.readFileSync(filePath).toString();
+
+    return md.replace(/\$ASSETS_URL\$/g, baseurl);
+  }
+}
--- api/src/engine/engine.controller.ts
+++ api/src/engine/engine.controller.ts
@@ -1,57 +1,59 @@
 import {
   Controller,
-  Delete,
   Get,
   Inject,
+  NotFoundException,
   Param,
-  Patch,
   Post,
+  Req,
+  Res,
   UseInterceptors,
 } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { join } from 'path/posix';
 import { Observable } from 'rxjs';
+import { AssetsService } from './assets.service';
 import { ENGINE_SERVICE } from './engine.constants';
 import { IEngineService } from './engine.interfaces';
 import { ErrorsInterceptor } from './interceptors/errors.interceptor';
+
 @UseInterceptors(ErrorsInterceptor)
 @Controller()
 export class EngineController {
   constructor(
     @Inject(ENGINE_SERVICE) private readonly engineService: IEngineService,
+    private readonly assetsService: AssetsService,
   ) {}
+
+  @Get('assets/:name')
+  getFile(
+    @Req() request: Request,
+    @Res() response: Response,
+    @Param('name') filename: string,
+  ) {
+    if (filename.endsWith('.md')) {
+      const baseurl =
+        request.protocol +
+        '://' +
+        join(request.get('host'), process.env.BASE_URL_CONTEXT ?? '', 'assets'); // not full url, should consider "/services"
+      const text = this.assetsService.getMarkdown(filename, baseurl);
+      response.setHeader('Content-Type', 'text/markdown');
+      return response.send(text);
+    }
+
+    const filepath = this.assetsService.getAssetFile(filename);
+
+    // Test if the file exist, if not send 404
+    if (filepath) {
+      return response.sendFile(filepath);
+    } else {
+      throw new NotFoundException();
+    }
+  }
 
   @Get('/algorithms')
   getAlgorithms(): Observable<string> | string {
     return this.engineService.getAlgorithmsREST();
-  }
-
-  @Get('/experiments')
-  getExperiments(): Observable<string> | string {
-    return this.engineService.getExperiments();
-  }
-
-  @Get('/experiments/:id')
-  getExperiment(@Param('id') id: string): Observable<string> | string {
-    return this.engineService.getExperimentREST(id);
-  }
-
-  @Delete('/experiments/:id')
-  deleteExperiment(@Param('id') id: string): Observable<string> | string {
-    return this.engineService.deleteExperiment(id);
-  }
-
-  @Patch('/experiments/:id')
-  editExperiment(@Param('id') id: string): Observable<string> | string {
-    return this.engineService.editExperimentREST(id);
-  }
-
-  @Post('experiments/transient')
-  startExperimentTransient(): Observable<string> | string {
-    return this.engineService.startExperimentTransient();
-  }
-
-  @Post('experiments')
-  startExperiment(): Observable<string> | string {
-    return this.engineService.startExperiment();
   }
 
   @Get('activeUser')
