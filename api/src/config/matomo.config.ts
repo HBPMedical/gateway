11b3258f93c8483a2296f8e7fb56454a21c6de69
stevereis <stevereis93@gmail.com>
2022-04-15 14:27:42 +0200

    wip: fix undefined field

--- api/src/config/matomo.config.ts
+++ api/src/config/matomo.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('matomo', () => {
   return {
     enabled: process.env.MATOMO_ENABLED || false,
-    urlBase: process.env.MAMOTO_URL || '',
+    urlBase: process.env.MAMOTO_URL || undefined,
     siteId: process.env.MATOMO_SITEID || undefined,
   };
 });
