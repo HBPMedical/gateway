ad573ec67a88148158457d03659f84aebbdb16bc
Steve Reis <stevereis93@gmail.com>
2022-04-20 11:50:44 +0200

    fix: Typo in matomo configuration file

--- api/src/config/matomo.config.ts
+++ api/src/config/matomo.config.ts
@@ -1,9 +1,10 @@
 import { registerAs } from '@nestjs/config';
+import { parseToBoolean } from 'src/common/utilities';
 
 export default registerAs('matomo', () => {
   return {
-    enabled: process.env.MATOMO_ENABLED || false,
-    urlBase: process.env.MAMOTO_URL || undefined,
-    siteId: process.env.MATOMO_SITEID || undefined,
+    enabled: parseToBoolean(process.env.MATOMO_ENABLED, false),
+    urlBase: process.env.MATOMO_URL || undefined,
+    siteId: process.env.MATOMO_SITE_ID || undefined,
   };
 });
