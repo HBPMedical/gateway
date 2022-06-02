5050798e4da49b5ab883fee43a765e47723ebda3
Steve Reis <stevereis93@gmail.com>
2022-06-02 14:17:18 +0000

    fix: User automatically extended in the request Homogenizing accessing active user

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersInterceptor } from './interceptors/users.interceptor';
 import { User } from './models/user.model';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersResolver, UsersService],
+  providers: [
+    UsersResolver,
+    UsersService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: UsersInterceptor,
+    },
+  ],
 })
 export class UsersModule {}
