34c18613e1d3dd9d9285843f539fcb41fe9d00b0
Steve Reis <stevereis93@gmail.com>
2022-06-20 14:08:36 +0200

    feat: Added configurable log levels

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './main/app.module';
 import * as cookieParser from 'cookie-parser';
+import { getLogLevels } from './common/utils/shared.utils';
+import { AppModule } from './main/app.module';
 
 const CORS_URL = process.env.CORS_URL ?? process.env.ENGINE_BASE_URL;
+const DEFAULT_LEVEL = process.env.NODE_ENV === 'production' ? 1 : 4;
+const LOG_LEVEL = process.env.LOG_LEVEL
+  ? parseInt(process.env.LOG_LEVEL)
+  : DEFAULT_LEVEL;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -15,6 +20,7 @@ async function bootstrap() {
         CORS_URL,
       ],
     },
+    logger: getLogLevels(LOG_LEVEL),
   });
 
   app.use(cookieParser());
