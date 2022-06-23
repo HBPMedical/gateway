d11cf01e10b19d2240ba3e82cf71d891769cfb73
Steve Reis <stevereis93@gmail.com>
2022-06-23 17:25:40 +0200

    fix: Issue with env type variable

--- api/src/config/cache.config.ts
+++ api/src/config/cache.config.ts
@@ -2,9 +2,11 @@ import { registerAs } from '@nestjs/config';
 import { parseToBoolean } from 'src/common/utils/shared.utils';
 
 export default registerAs('cache', () => {
+  const max = process.env.CACHE_MAX_ITEMS;
+  const ttl = process.env.CACHE_TTL;
   return {
     enabled: parseToBoolean(process.env.CACHE_ENABLED, false),
-    ttl: process.env.CACHE_TTL || undefined,
-    max: process.env.CACHE_MAX_ITEMS || undefined,
+    ttl: ttl ? parseInt(ttl) : undefined,
+    max: max ? parseInt(max) : undefined,
   };
 });
