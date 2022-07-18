ee07a23d0c5adc1808745f6b94cc8eeacc96e980
Steve Reis <stevereis93@gmail.com>
2022-07-18 16:27:10 +0200

    feat: Linear regression (CV) integrated

--- api/src/engine/connectors/exareme/handlers/index.ts
+++ api/src/engine/connectors/exareme/handlers/index.ts
@@ -2,21 +2,24 @@ import { Domain } from 'src/engine/models/domain.model';
 import { Experiment } from '../../../../engine/models/experiment/experiment.model';
 import AnovaOneWayHandler from './algorithms/anova-one-way.handler';
 import DescriptiveHandler from './algorithms/descriptive.handler';
+import LinearRegressionCVHandler from './algorithms/linear-regression-cv.handler';
 import LinearRegressionHandler from './algorithms/linear-regression.handler';
 import PCAHandler from './algorithms/PCA.handler';
 import PearsonHandler from './algorithms/pearson.handler';
 import RawHandler from './algorithms/raw.handler';
+import ResultHandler from './result-handler.interface';
 
-const start = new PearsonHandler();
+const start = new PearsonHandler() as ResultHandler;
 
 start
   .setNext(new DescriptiveHandler())
   .setNext(new AnovaOneWayHandler())
   .setNext(new PCAHandler())
   .setNext(new LinearRegressionHandler())
+  .setNext(new LinearRegressionCVHandler())
   .setNext(new RawHandler()); // should be last handler as it works as a fallback (if other handlers could not process the results)
 
 export default (exp: Experiment, data: unknown, domain: Domain): Experiment => {
-  start.handle(exp, data);
+  start.handle(exp, data, domain);
   return exp;
 };
