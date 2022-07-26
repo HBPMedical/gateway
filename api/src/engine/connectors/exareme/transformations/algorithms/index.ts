6a10f157dcf0d3ba0e459cbca4f97e086a40305d
Steve Reis <stevereis93@gmail.com>
2022-07-26 14:31:26 +0200

    fix(exareme): Linear reg cv adaptation

--- api/src/engine/connectors/exareme/transformations/algorithms/index.ts
+++ api/src/engine/connectors/exareme/transformations/algorithms/index.ts
@@ -12,7 +12,7 @@ const transformToAlgorithms = jsonata(`
     'LOGISTIC_REGRESSION','TTEST_INDEPENDENT','TTEST_PAIRED',
     'PEARSON','ID3','KMEANS','NAIVE_BAYES',
     'TTEST_ONESAMPLE','PCA','CALIBRATION_BELT','CART',
-    'KAPLAN_MEIER','THREE_C', 'ONE_WAY_ANOVA'];
+    'KAPLAN_MEIER','THREE_C', 'ONE_WAY_ANOVA', 'PEARSON', 'LINEAR_REGRESSION_CV'];
     $linkedVars:= ['positive_level', 'negative_level', 'outcome_neg', 'outcome_pos'];
     $linkedCoVars:= ['referencevalues', 'xlevels'];
     $truthy:= function($val) {(
